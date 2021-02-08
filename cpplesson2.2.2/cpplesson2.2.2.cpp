36a381ec909e1a0c9723fefe0a7af3684a07a480
VladislavErekeshov <tommysyching@gmail.com>
2021-02-08 20:28:12 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.2.2/cpplesson2.2.2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << d << endl;
+	cout << c << endl;
+	cout << b << endl;
+	cout << a << endl;
+	return 0;
+}
